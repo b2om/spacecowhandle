7dbc14380d2e5eb8976c814382033fcb69564b9a
b2om <m.j.s.boom@students.uu.nl>
2015-11-19 12:47:05 +0100

    Added the project
    this is the first build of the project. It might not work because of the
    SDK destination.

---
+++ Project/desktop/src/com/unbeatable/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.unbeatable.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.unbeatable.MyGdxGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MyGdxGame(), config);
+	}
+}
